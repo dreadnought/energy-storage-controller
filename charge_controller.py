70ba9657e669c3d24cab5844ec21e0a80938e9e2
Patrick Salecker <mail@salecker.org>
2020-03-04 21:31:18 +0100

    Charge controller executable

---
+++ charge_controller.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from pyedimax.smartplug import SmartPlug
+
+from config import config
+from controller.charge_controller import ChargeController
+from logger import get_logger
+from metrics import Metrics
+
+logger = get_logger(level='info')
+
+smart_plug_auth = (config['charger']['smartplug_username'], config['charger']['smartplug_password'])
+smart_plug = SmartPlug(config['charger']['smartplug_ip'], smart_plug_auth)
+
+m = Metrics(database_name=config['influxdb']['database_name'])
+cc = ChargeController(config=config, logger=logger, metrics=m, smart_plug=smart_plug)
+volt = cc.pwm.get_pwm_volt()
+logger.info('%0.2f volt at start' % volt)
+
+try:
+    cc.loop()
+except KeyboardInterrupt:
+    cc.stop()
