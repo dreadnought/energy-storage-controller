07164a9be3d787476f3a3933b23cfe1b28b506dc
Patrick Salecker <mail@salecker.org>
2020-05-01 12:31:57 +0200

    Use logging in inverter controller

--- controller/inverter_controller.py
+++ controller/inverter_controller.py
@@ -17,45 +17,49 @@ tz = pytz.timezone('Europe/Berlin')
 
 
 class InverterController():
-    def __init__(self, config):
+    def __init__(self, config, logger):
         self.config = config
-        print('init...', flush=True)
+        self.logger = logger
+        self.logger.info('init...')
         self.metrics = Metrics(database_name=self.config['influxdb']['database_name'])
-        print('energy meter...', flush=True)
+        self.logger.info('energy meter...')
         self.energy_meter = SMAEnergyManagerThread(serial_number=config['sma_energy_manager']['serial_number'],
                                                    metrics=self.metrics)
         self.energy_meter.start()
-        print('battery inverter...', flush=True)
-        self.battery_inverter = AEConversionInverterThread(config=config['aeconversion_inverter'], metrics=self.metrics)
+        self.logger.info('battery inverter...')
+        self.battery_inverter = AEConversionInverterThread(config=config['aeconversion_inverter'],
+                                                           metrics=self.metrics,
+                                                           logger=self.logger)
         self.battery_inverter.start()
-        print('smart plug...', flush=True)
+        self.logger.info('smart plug...')
         self.smart_plug = SmartPlug(config['charger']['smartplug_ip'],
                                     (config['charger']['smartplug_username'], config['charger']['smartplug_password']))
 
-        self.battery_inverter_relay_ac = GpioPin(pin=64)
+        self.battery_inverter_relay_ac = GpioPin(pin=config['aeconversion_inverter']['gpio_pin'])
 
-        print('init done', flush=True)
+        self.logger.info('init done')
 
     def go_idle(self):
-        print('going idle')
-        self.battery_inverter_relay_ac.set_state(False)
-        #self.battery_charger_relay_ac.off()
+        if self.battery_inverter_relay_ac.get_state() == 1:
+            self.logger.info('Turning inverter relay off')
+            self.battery_inverter_relay_ac.set_state(False)
+        self.logger.debug('Inverter relay off')
 
     def loop_run(self):
-        print('==== start of run %s ====' % datetime.now(tz), flush=True)
+        self.logger.debug('==== start of run %s ====' % datetime.now(tz))
         watt_tolerance = 20
         watt_inverter_start = 100
         if not self.energy_meter.is_healthy():
-            print('no data from energy meter')
+            self.logger.error('no data from energy meter')
             self.go_idle()
             return False
 
         em_import = self.energy_meter.data['p_import']
         em_export = self.energy_meter.data['p_export']
         em_balanced = False
-        print('%s to, %s from grid' % (em_export, em_import))
+        self.logger.debug('%s to, %s from grid' % (em_export, em_import))
         if em_export + em_import < watt_tolerance:
-            print('balanced')
+            self.logger.debug('balanced')
             em_balanced = True
         elif em_export == 0.0 and em_import > 0.0:
             # print('pulling energy from grid')
@@ -64,34 +68,34 @@ class InverterController():
             # print('pushing energy to grid')
             pass
         else:
-            print('undefined state')
+            self.logger.error('undefined state')
 
         battery_charger_on = False
 
         inverter_in_operation = False
         if not self.battery_inverter.is_healthy():
-            print('inverter unhealthy, turning it off')
+            self.logger.error('Inverter thread unhealthy')
             self.go_idle()
             return
 
         # print('battery inverter connected')
-        print('Inverter', self.battery_inverter.data)
+        self.logger.debug('Inverter', self.battery_inverter.data)
         battery_status = self.check_battery_discharge()
         battery_level = 100 / (self.config['battery']['max_voltage'] - self.config['battery']['min_voltage']) * (
                 self.battery_inverter.data['pv_volt'] - self.config['battery']['min_voltage'])
-        print("Battery Level: %0.2f%%" % battery_level)
+        self.logger.debug("Battery Level: %0.2f%%" % battery_level)
 
         max_discharge_watt = self.config['battery']['max_discharge_watt']
         if battery_level < 20:
-            print('battery level <20%, limiting discharge')
+            self.logger.info('battery level <20%, limiting discharge')
             max_discharge_watt = max_discharge_watt / 2
 
         if not battery_status or battery_charger_on:
-            print('battery not good, skipping rest')
+            self.logger.info('battery not good, skipping rest')
             self.go_idle()
             return
         elif battery_level < 0.0:
-            print('battery low, turning inverter off')
+            self.logger.info('battery low, turning inverter off')
             self.battery_inverter_relay_ac.set_state(False)
             return
 
@@ -99,34 +103,36 @@ class InverterController():
         last_limit = self.battery_inverter.inverter.last_limit
         if em_export > inverter_max:
             self.battery_inverter_relay_ac.set_state(False)
-            print("Off, to much export")
+            self.logger.debug("Off, to much export")
         elif last_limit and last_limit == 10:
             self.battery_inverter_relay_ac.set_state(False)
-            print("Off, last limit 10")
+            self.logger.info("Off, last limit 10")
             self.battery_inverter.inverter.last_limit = 0
         elif self.battery_inverter.inverter.is_active():
             inverter_in_operation = True
-            print("On")
+            self.logger.debug("On")
         else:
-            print("Off", flush=True)
+            self.logger.debug("Off")
             now = datetime.now(tz)
             if now.hour > 11 and now.hour < 16:
-                print('charging time, not activating')
+                self.logger.debug('charging time, not activating')
             elif self.smart_plug.state == 'ON':
                 print("Charger is running, not activating inverter", flush=True)
                 return
-            elif battery_level > 40 and em_import > watt_inverter_start:
-                # turn on inverter AC if the battery is at least half filled and we need energy
-                print("turning on inverter")
+            elif battery_level > 25 and em_import > watt_inverter_start:
+                # turn on inverter AC if the battery is at least 40% and we need energy
+                self.logger.info("Turning inverter on")
                 self.battery_inverter_relay_ac.set_state(True)
                 time.sleep(10)
                 self.battery_inverter.queue_command(command='set_limit', args={'limit': 100})
                 return
             else:
-                print("No reason to activate inverter")
+                self.logger.info("No reason to activate inverter")
 
-        if inverter_in_operation and (not em_balanced or time.time() - self.battery_inverter.inverter.last_limit_change > 60 * 4):
-            print('requesting energy')
+        if not inverter_in_operation:
+            pass
+        elif not em_balanced or time.time() - self.battery_inverter.inverter.last_limit_change > 60 * 4:
+            self.logger.info('Requesting energy')
             # todo: check vs. watt_max
             self.battery_inverter.queue_command("request_energy",
                                                 {
@@ -136,26 +142,31 @@ class InverterController():
                                                     'watt_max': max_discharge_watt,
                                                 })
 
-        print('==== end of run ====', flush=True)
+        self.logger.debug('==== end of run ====')
 
     def check_battery_discharge(self):
         if self.battery_inverter.data['pv_volt'] > self.config['battery']['min_voltage']:
             return True
 
-        print("limiting")
+        self.logger.info("limiting")
         if self.battery_inverter.data['ac_watt'] > 510:
-            print("invalid ac_watt value")
+            self.logger.warning("invalid ac_watt value")
             new_limit = 100
         elif self.battery_inverter.inverter.last_limit:
             new_limit = self.battery_inverter.inverter.last_limit - config['aeconversion_inverter']['limit_step']
         else:
             new_limit = self.battery_inverter.data['ac_watt'] - config['aeconversion_inverter']['limit_step']
         if new_limit < 10:
-            print("low voltage")
+            self.logger.warning("low voltage")
             return False
         # self.battery_inverter.queue_command(('set_limit', {'limit': new_limit}))
         time.sleep(5)
         return True
+
+    def stop(self):
+        self.energy_meter.stop()
+        self.battery_inverter.stop()
+        self.battery_inverter_relay_ac.set_state(False)
 
     def loop(self):
         while True:
@@ -165,7 +176,4 @@ class InverterController():
             except KeyboardInterrupt:
                 break
 
-        self.energy_meter.stop()
-        self.battery_inverter.stop()
-        self.battery_inverter_relay_ac.set_state(False)
-
+        self.stop()
--- inverter_controller.py
+++ inverter_controller.py
@@ -4,8 +4,11 @@ import time
 
 from config import config
 from controller.inverter_controller import InverterController
+from logger import get_logger
+
+logger = get_logger(level='info')
 
 
-controller = InverterController(config=config)
+controller = InverterController(config=config, logger=logger)
 time.sleep(20)  # give the threads time to start, connect and receive data
 controller.loop()
